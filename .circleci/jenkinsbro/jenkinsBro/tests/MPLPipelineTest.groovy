147896b0f8a65c023ad02e176260890d426c5d50
Sergei Parshev <sparshev@griddynamics.com>
2020-08-11 23:03:04 -0700

    MPL-77 Fixed circleci forked repo usage

--- .circleci/jenkinsbro/jenkinsBro/tests/MPLPipelineTest.groovy
+++ .circleci/jenkinsbro/jenkinsBro/tests/MPLPipelineTest.groovy
@@ -2,8 +2,9 @@
  * MPL build using MPL
  *
  * Parameters:
- *   mpl_build_test.clone_url - where to get the sources
- *   mpl_build_test.branch_spec - branch/commit/tag to build
+ *   mpl_build_test.clone_url      - where to get the sources
+ *   mpl_build_test.ref_spec       - refspec mapping
+ *   mpl_build_test.branch_spec    - branch/commit/tag to build
  *   mpl_build_test.credentials_id - if the creds are required
  */
 
@@ -16,15 +17,17 @@ import org.junit.Test
 class MPLPipelineTest extends JenkinsTest {
   @Test
   void testJenkinsfileBuild() {
-    def scm = new GitSCM(MODULE.clone_url)
-    scm.branches = [new BranchSpec(MODULE.branch_spec ?: 'master')]
-    scm.userRemoteConfigs = GitSCM.createRepoList(MODULE.clone_url, MODULE.credentials_id ?: '')
+    println("Setup job configuration: ${MODULE}")
+
+    def remote_configs = [new UserRemoteConfig(MODULE.clone_url, null, MODULE.ref_spec, MODULE.credentials_id ?: '')]
+    def branches = [new BranchSpec(MODULE.branch_spec ?: 'master')]
+    def scm = new GitSCM(remote_configs, branches, false, [], null, null, [])
 
     def wfjob = jenkins.createProject(WorkflowJob.class, 'mpl-build')
     def flow_def = org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition.newInstance(scm, 'Jenkinsfile')
     wfjob.setDefinition(flow_def)
 
-    // Running the job and wait for complete
+    println('Running the job and wait for complete')
     def task_future = wfjob.scheduleBuild2(0)
     def build = task_future.waitForStart()
     task_future.get() // Waiting
