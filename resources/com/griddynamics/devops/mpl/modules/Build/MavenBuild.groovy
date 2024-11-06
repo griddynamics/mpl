9a305070632b6679ef92e237ca95adb1e4aa42a3
Sergei Parshev <sergei@parshev.net>
2024-11-05 19:44:46 -0500

    Since jenkins moved to jdk17 - MaxPermSize is removed from it

--- resources/com/griddynamics/devops/mpl/modules/Build/MavenBuild.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Build/MavenBuild.groovy
@@ -4,5 +4,5 @@
 
 withEnv(["PATH+MAVEN=${tool(CFG.'maven.tool_version' ?: 'Maven 3')}/bin"]) {
   def settings = CFG.'maven.settings_path' ? "-s '${CFG.'maven.settings_path'}'" : ''
-  sh """mvn -B ${settings} -DargLine='-Xmx1024m -XX:MaxPermSize=1024m' clean install"""
+  sh """mvn -B ${settings} -DargLine='-Xmx1024m' clean install"""
 }
